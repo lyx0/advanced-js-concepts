ebbb1fb13601169c477ddaa6de27952155b605d0
Lyx0 <5349411-Lyx0@users.noreply.gitlab.com>
2020-12-11 13:55:06 +0100

    start section4

---
+++ Section4/JavascriptTypes/main.js
@@ -0,0 +1,23 @@
+
+// number
+typeof 5
+
+// boolean
+typeof true
+
+// string
+typeof 'To be or not to be'
+
+// undefined
+typeof undefined
+
+// !!! Object
+typeof null 
+
+// symbol
+// Symbol value is useful for identifying an 
+// object so that the object value is unique
+typeof Symbol('just me')
+
+// function
+typeof {}
---
+++ Section4/README.md
@@ -0,0 +1,14 @@
+All programming languages have types, the building blocks of the language
+Strings, Booleans and more
+
+Two Types, 
+    Dynamically Typed
+  and 
+    Statically Typed
+Languages
+
+Pass by reference vs pass by value
+
+Type coercion
+
+Arrays, Functions, Objects
