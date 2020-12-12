1ec4d17b26cdd0f0933283ac16f4857e4e8e5ee9
Lyx0 <5349411-Lyx0@users.noreply.gitlab.com>
2020-12-12 20:33:52 +0100

    start section7

---
+++ Section7/ExerciseAmazon/main.js
@@ -0,0 +1,19 @@
+// Amazon shopping
+const user = {
+  name: 'Kim',
+  active: true,
+  cart: [],
+  purchases: []
+}
+
+/*
+ Implement a cart feature:
+   1. Add items to cart.
+   2. Add 3% tax to item in cart.
+   3. Buy item: cart --> purchases.
+   4. Empty cart
+
+ Bonus:
+   Accept refunds.
+   Track user history.
+*/
---
+++ Section7/FunctionalProgrammingIntroduction/main.js
@@ -0,0 +1,13 @@
+/*
+ Functional programming is all about seperating concerns
+
+ Seperating our code in seperate chunks so that everythings well organized and everything
+ in our code concers itself with the bit it's good at
+
+ Functions operate on well defined data structures like arrays or objects rather than belonging
+ to that data structure
+
+ Pure Functions:
+    Seperation between data of a program and the behavior of a program
+    Once something is created it cant be changed and we adhere to the principle of pure function
+*/
