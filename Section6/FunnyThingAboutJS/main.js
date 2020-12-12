700660473212e92f7301b7fd88c9235dc4233f9d
lyx0 <66651385+lyx0@users.noreply.github.com>
2020-12-12 18:10:00 +0100

    xd

---
+++ Section6/FunnyThingAboutJS/main.js
@@ -0,0 +1,12 @@
+var a = new Number(5)
+typeof a 
+// Object
+var b = 5
+typeof b 
+// number
+a === b
+// false
+a == b
+// true
+
+new Date().getFullYear
--- Section6/MoreConstructorFunctions/main.js
+++ Section6/MoreConstructorFunctions/main.js
@@ -3,14 +3,17 @@
 function Elf(name, weapon) {
   this.name = name;
   this.weapon = weapon;
+  var a = 5;
 }
 
 Elf.prototype.attack = function() {
   return console.log(this.name + ' attacks with ' + this.weapon)
 }
+Elf.prototype.build = function() {
+  return console.log(this.name + ' builds a house')
+}
 
 const peter = new Elf('Peter', 'stones')
+peter.build()
 const sam = new Elf('Sam', 'fire')
-// console.log(sam.name)
 sam.attack()
-peter.attack()
